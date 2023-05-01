ce2a44c20d782abd325857db49b67bb3d9e4046a
PawelFats <132273276+PawelFats@users.noreply.github.com>
2023-05-01 19:52:10 +0600

    Update hello.c

--- 1LR/hello.c
+++ 1LR/hello.c
@@ -2,8 +2,8 @@
 #include <linux/kernel.h> // included for KERN_INFO
 #include <linux/init.h> // included for __init and __exit macros
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Lakshmanan");
+MODULE_LICENSE("NGTU");
+MODULE_AUTHOR("PavelFatyanov");
 MODULE_DESCRIPTION("A Simple Hello World module");
 
 static int __init hello_init(void)
